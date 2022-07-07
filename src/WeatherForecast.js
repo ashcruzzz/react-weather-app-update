2cbeb76cdb4ac12f7664ce4f963616a0429bab02
ashcruzzz <ashleycruzart@gmail.com>
2022-07-07 16:39:48 -0700

    added forecast 5 day row

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -16,6 +16,15 @@ export default function WeatherForecast(props) {
     return (
       <div className="weatherForecast">
         <div className="row">
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            }
+          })}
           <div className="col">
             <WeatherForecastDay data={forecast[0]} />
           </div>
